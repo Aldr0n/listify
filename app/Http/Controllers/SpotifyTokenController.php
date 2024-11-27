d72a5c499e0a7e4a7979cea9f9fa99058fd9c8c1
Ruben Maurer <aldr0n@protonmail.com>
2024-11-27 20:08:55 +0100

    feat(spotifyToken): generate SpotifyToken model

---
+++ app/Http/Controllers/SpotifyTokenController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SpotifyToken;
+use App\Http\Requests\StoreSpotifyTokenRequest;
+use App\Http\Requests\UpdateSpotifyTokenRequest;
+
+class SpotifyTokenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSpotifyTokenRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SpotifyToken $spotifyToken)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SpotifyToken $spotifyToken)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSpotifyTokenRequest $request, SpotifyToken $spotifyToken)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SpotifyToken $spotifyToken)
+    {
+        //
+    }
+}
