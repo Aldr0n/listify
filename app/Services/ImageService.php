bb5bb921202383ed7d4be677c6ed9c1081af2bbc
Ruben Maurer <aldr0n@protonmail.com>
2024-11-28 20:22:06 +0100

    feat(imageService): add ImageService scaffold

---
+++ app/Services/ImageService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class ImageService
+{
+    public function storeImage(int $userId, array $image)
+    {
+        // TODO: Implement storeImage() method.
+    }
+
+    public function getImage(int $userId, string $imageId)
+    {
+        // TODO: Implement getImage() method.
+    }
+}
