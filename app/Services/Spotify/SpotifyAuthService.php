97b5f2b56ff8eac513862c885da1c9051aa7c2e4
Ruben Maurer <aldr0n@protonmail.com>
2024-11-28 21:51:16 +0100

    refactor(spotify): relocate Spotify services location

--- app/Services/Spotify/SpotifyAuthService.php
+++ app/Services/Spotify/SpotifyAuthService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Spotify;
 
 use App\Contracts\Services\OauthTokenService;
 use App\Models\SpotifyToken;
--- app/Services/Spotify/SpotifyUserService.php
+++ app/Services/Spotify/SpotifyUserService.php
@@ -2,9 +2,10 @@
 
 namespace App\Services;
 
+use App\Contracts\Services\AppUserService;
 use App\Contracts\Services\OauthTokenService;
 
-class SpotifyUserService
+class SpotifyUserService implements AppUserService
 {
     public function __construct(
         protected OauthTokenService $spotifyApiService,
@@ -13,5 +14,10 @@ class SpotifyUserService
     public function storeUserProfile(int $userId)
     {
         // TODO: Implement storeUserProfile() method.
+    }
+
+    public function deleteUserProfile(int $userId)
+    {
+        // TODO: Implement deleteUserProfile() method.
     }
 }
