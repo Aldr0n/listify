2d61c95bb681f6b05a391f7f041054b8e4498a7f
Ruben Maurer <aldr0n@protonmail.com>
2024-11-28 01:46:54 +0100

    chore(vite.config): update Vite configuration for https usage

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "listify.test",
+        hmr: {
+            host: "listify.test",
+        },
+        cors: true,
+    },
 });
