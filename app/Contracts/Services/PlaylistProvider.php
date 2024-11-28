db33b3976973a52e1cb1e1bd36e8e033701a36df
Ruben Maurer <aldr0n@protonmail.com>
2024-11-28 20:05:53 +0100

    refactor(playlistProvider): change return types to use correct models

--- app/Contracts/Services/PlaylistProvider.php
+++ app/Contracts/Services/PlaylistProvider.php
@@ -2,16 +2,19 @@
 
 namespace App\Contracts\Services;
 
+use App\Models\Playlist;
+use Illuminate\Database\Eloquent\Collection;
+
 interface PlaylistProvider
 {
     /**
      * Get all user playlists
      */
-    public function getPlaylists(int $userId): array;
+    public function getPlaylists(int $userId): Collection;
 
     /**
      * Get a specific user playlist
      */
-    public function getPlaylist(int $userId, string $playlistId): array;
+    public function getPlaylist(int $userId, string $playlistId): Playlist;
 }
 
