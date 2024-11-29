6c537792562130bd5640e2848755a71acb7a8d33
Ruben Maurer <aldr0n@protonmail.com>
2024-11-29 21:59:34 +0100

    feat(tracks): add tracks table with playlist relationship

--- database/migrations/2024_11_27_174053_create_tracks_table.php
+++ database/migrations/2024_11_27_174053_create_tracks_table.php
@@ -11,9 +11,34 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tracks', function (Blueprint $table) {
+        Schema::create('tracks', function (Blueprint $table)
+        {
             $table->id();
             $table->timestamps();
+            $table->string('spotify_id')->unique();
+            $table->string('name');
+            $table->integer('duration_ms');
+            $table->json('artists')->nullable();
+            $table->json('album')->nullable();
+            $table->string('thumbnail_url')->nullable();
+            $table->string('href')->nullable();
+            $table->integer('popularity')->nullable();
+            $table->integer('track_number')->nullable();
+            $table->boolean('explicit')->nullable();
+            $table->json('available_markets')->nullable();
+        });
+
+        /*
+         * Pivot table for playlist <-> track relationship
+         */
+        Schema::create('playlist_track', function (Blueprint $table)
+        {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('playlist_id')->constrained('playlists')->onDelete('cascade');
+            $table->foreignId('track_id')->constrained('tracks')->onDelete('cascade');
+
+            $table->unique(['playlist_id', 'track_id']);
         });
     }
 
@@ -23,5 +48,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('tracks');
+        Schema::dropIfExists('playlist_track');
     }
 };
