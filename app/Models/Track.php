b8d92e55996297c73294e9d200f1db362973be5d
Ruben Maurer <aldr0n@protonmail.com>
2024-11-29 17:31:37 +0100

    feat(track): implements model relationships

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Track extends Model
 {
     /** @use HasFactory<\Database\Factories\TrackFactory> */
     use HasFactory;
+
+    protected $fillable = ['id', 'name', 'artist', 'album', 'thumbnail_url'];
+
+    public function playlist(): BelongsToMany
+    {
+        return $this->belongsToMany(Playlist::class);
+    }
 }
