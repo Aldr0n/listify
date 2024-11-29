93bb7ca58a255b31ea594b7fc3c32f5603b7def0
Ruben Maurer <aldr0n@protonmail.com>
2024-11-29 17:36:10 +0100

    refactor(apiClientService): extract API client service interface

---
+++ app/Contracts/Services/ApiClientService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Contracts\Services;
+
+use App\Models\SpotifyToken;
+
+interface ApiClientService
+{
+    public function fetchUserPlaylists(string $spotifyUserId, SpotifyToken $token): array;
+
+    public function fetchUserProfile(string $spotifyUserId, SpotifyToken $token): array;
+
+    public function fetchPlaylistTracks(string $playlistId): array;
+
+    public function refreshToken(int $userId): array;
+}
+
