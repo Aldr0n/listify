33c2ce4ae48800d3c6b4c420ef8d52eacb70c812
Ruben Maurer <aldr0n@protonmail.com>
2024-12-01 09:41:48 +0100

    refactor(ui): moves Spotify controls to navigation bar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,6 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="flex flex-row items-center gap-4 p-6 text-gray-900 ">
-                    <livewire:spotify.spotify-connect />
-                    <livewire:spotify.sync-playlists />
                 </div>
             </div>
         </div>
