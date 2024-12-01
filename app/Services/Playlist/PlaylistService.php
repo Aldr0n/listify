f1871825a26f2f1490b108a888a2a706c92ddae1
Ruben Maurer <aldr0n@protonmail.com>
2024-12-01 09:21:45 +0100

    add(playlist): implement thumbnail handling

--- app/Services/Playlist/PlaylistService.php
+++ app/Services/Playlist/PlaylistService.php
@@ -3,12 +3,18 @@
 namespace App\Services\Playlist;
 
 use App\Contracts\Provider\PlaylistProvider;
+use App\Enums\MediaType;
 use App\Models\Playlist;
+use App\Services\ImageService;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
 
 class PlaylistService implements PlaylistProvider
 {
+    public function __construct(
+        protected ImageService $imageService,
+    ) {}
+
     public function getPlaylist(int $userId, string $playlistId): Playlist
     {
         return Playlist::where('user_id', $userId)->find($playlistId);
@@ -21,21 +27,34 @@ class PlaylistService implements PlaylistProvider
 
     public function resolvePlaylist(array $playlistData): Playlist
     {
-        $spotifyId = $playlistData['id'];
+        $spotifyId     = $playlistData['id'];
+        $thumbnail_id  = NULL;
+        $thumbnail_url = $playlistData['images'][0]['url'] ?? NULL;
 
         if (!$spotifyId) {
             throw new \RuntimeException('Missing required spotify_id');
         }
 
+        if ($thumbnail_url) {
+            $thumbnail_id = $this->handleThumbnail($thumbnail_url);
+        }
+
+        $playlist = [
+            'title'        => $playlistData['name'],
+            'description'  => $playlistData['description'] ?? NULL,
+            'thumbnail_id' => $thumbnail_id,
+            'user_id'      => $playlistData['user_id'] ?? Auth::id(),
+            'map'          => $playlistData['map'] ?? NULL,
+        ];
+
         return Playlist::firstOrCreate(
             ['spotify_id' => $spotifyId],
-            [
-                'title'         => $playlistData['name'],
-                'description'   => $playlistData['description'] ?? NULL,
-                'thumbnail_url' => $playlistData['images'][0]['url'] ?? NULL,
-                'user_id'       => $playlistData['user_id'] ?? Auth::id(),
-                'map'           => $playlistData['map'] ?? NULL,
-            ]
+            $playlist
         );
+    }
+
+    public function handleThumbnail(string $thumbnailUrl): string
+    {
+        return $this->imageService->downloadImage($thumbnailUrl, MediaType::ALBUM_THUMBNAIL);
     }
 }
