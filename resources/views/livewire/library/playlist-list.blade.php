146078e1b8daa5d0d8af234fbc0e9d9188398e5a
Ruben Maurer <aldr0n@protonmail.com>
2024-12-02 04:43:06 +0100

    feat(ui): enhance design

--- resources/views/livewire/library/playlist-list.blade.php
+++ resources/views/livewire/library/playlist-list.blade.php
@@ -1,24 +1,27 @@
-<div class="flex flex-col gap-6">
+<div class="flex flex-col gap-3">
     <div>
         <input 
             wire:model.live.debounce.500ms="search" 
             type="text" 
             placeholder="Search playlists..." 
-            class="w-full px-4 py-2 border rounded-md"
+            class="w-full px-4 py-2 border-none rounded-md shadow"
         >
     </div>
-    <div 
-        @playlist-grid-update.window="$wire.$refresh()"
-        class="grid grid-cols-2 gap-2 min-h-[592px] content-start"
-    >
-        @foreach($playlists as $playlist)
-            <a href="{{ route('playlist.show', $playlist->id) }}" wire:key="playlist-{{ $playlist->id }}" class="flex flex-row items-center gap-6 p-4 bg-gray-100 rounded-md hover:bg-gray-200">
-                <img src="{{ $playlist->thumbnail_url }}" alt="" class="object-cover w-full rounded-md max-w-[80px] aspect-square">
-                <p>{{ $playlist->title }}</p>
-            </a>
-        @endforeach
-    </div>
-    <div wire:navigate.preserve-scroll>
-        {{ $playlists->links() }}
-    </div>
+    
+        <div class="flex flex-col gap-5 bg-white shadow sm:p-6 sm:rounded-lg">
+            <div 
+                @playlist-grid-update.window="$wire.$refresh()"
+                class="grid grid-cols-2 gap-2 min-h-[592px] content-start"
+            >
+                @foreach($playlists as $playlist)
+                    <a wire:navigate href="{{ route('playlist.show', $playlist->id) }}" wire:key="playlist-{{ $playlist->id }}" class="flex flex-row items-center gap-6 p-4 bg-gray-100 rounded-md hover:bg-gray-200">
+                        <img src="{{ $playlist->thumbnail_url }}" alt="" class="object-cover w-full rounded-md max-w-[80px] aspect-square">
+                        <p>{{ $playlist->title }}</p>
+                    </a>
+                @endforeach
+            </div>
+            <div wire:navigate.preserve-scroll>
+                {{ $playlists->links(data: ['scrollTo' => false]) }}
+            </div>
+        </div>
 </div>
--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -1,17 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Meine Playlists') }}
+        <h2 class="flex flex-row items-center justify-between gap-4 text-xl font-semibold leading-tight text-gray-800">
+            {{ __('My Playlists') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-            <div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
-                <div class="">
-                    <livewire:library.playlist-list />
-                </div>
-            </div>
+            <livewire:library.playlist-list />
         </div>
     </div>
 </x-app-layout>
