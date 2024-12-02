d4b265641ec3c866eedded6b91f9787e6c654df3
Ruben Maurer <aldr0n@protonmail.com>
2024-12-02 05:42:37 +0100

    docs(appUserService): add method documentation

--- app/Contracts/Services/AppUserService.php
+++ app/Contracts/Services/AppUserService.php
@@ -4,7 +4,16 @@ namespace App\Contracts\Services;
 
 interface AppUserService
 {
+    /**
+     * Store Spotify user profile
+     * @param array $spotifyUser
+     * @return void
+     */
     public function storeUserProfile(array $spotifyUser);
 
+    /**
+     * Delete current user profile
+     * @return void
+     */
     public function deleteUserProfile();
 }
