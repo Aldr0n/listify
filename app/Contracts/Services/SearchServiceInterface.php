b9d3541d4c9b9c91de11e8ae2e82e4b1b3217019
Ruben Maurer <aldr0n@protonmail.com>
2024-12-02 05:44:55 +0100

    docs(searchServiceInterface): add method documentation

--- app/Contracts/Services/SearchServiceInterface.php
+++ app/Contracts/Services/SearchServiceInterface.php
@@ -4,7 +4,25 @@ namespace App\Contracts\Services;
 
 interface SearchServiceInterface
 {
+    /**
+     * Execute search with given term
+     * @param string $term
+     * @param string $token
+     * @return array
+     */
     public function search(string $term, string $token): array;
+
+    /**
+     * Clean and format search term
+     * @param string $term
+     * @return string
+     */
     public function sanitizeSearchTerm(string $term): string;
+
+    /**
+     * Format raw search results
+     * @param array $results
+     * @return array
+     */
     public function formatResults(array $results): array;
 }
